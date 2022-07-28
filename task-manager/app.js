20c98cd47ca05d8f44bb5e42c8af86845917e6c2
Abishek Amstrong <abishekamstrong@gmail.com>
2022-07-28 22:50:55 +0530

    code revamping is done for the controllers

--- task-manager/app.js
+++ task-manager/app.js
@@ -3,14 +3,15 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config(); // To pass the connection string from env file
+const notFound = require('./middleware/not-found');
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json()); // parses incoming JSON requests and puts the parsed data in req.body.
 
 // routes
-
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
 // app.get('api/v1/tasks')  get all the tasks
 // app.post('api/v1/tasks')  To create a new task
--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,47 +1,101 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find({}); // Reference - https://mongoosejs.com/docs/queries.html
-        res.status(200).json({ tasks });
-    } catch (error) {
-        res.status(500).json({ msg: error });
+// const getAllTasks = async (req, res) => {
+//     try {
+//         const tasks = await Task.find({}); // Reference - https://mongoosejs.com/docs/queries.html
+//         res.status(200).json({ tasks });
+//     } catch (error) {
+//         res.status(500).json({ msg: error });
+//     }
+//     // res.send('get all tasks');
+// }
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+});
+
+// const createTask = async (req, res) => {
+//     try {
+//         const task = await Task.create(req.body);
+//         res.status(201).json({ task });
+//     } catch (error) {
+//         res.status(500).json({ msg: error });
+//     }
+//     // res.json(req.body);
+// }
+
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+});
+
+// const getTask = async (req, res) => {
+//     try {
+//         const { id: taskID } = req.params;
+//         const task = await Task.findOne({ _id: taskID }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOne
+
+//         if (!task) {
+//             return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
+//         }
+
+//         res.status(200).json({ task });
+//     } catch (error) {
+//         res.status(500).json({ msg: error });
+//     }
+//     // res.json({ id: req.params.id });
+// }
+
+const getTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOne
+
+    if (!task) {
+        return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
     }
-    // res.send('get all tasks');
-}
 
-const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(201).json({ task });
-    } catch (error) {
-        res.status(500).json({ msg: error });
+    res.status(200).json({ task });
+});
+
+// const updateTask = async (req, res) => {
+//     try {
+//         const { id: taskID } = req.params;
+//         const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+//             new: true,
+//             runValidators: true
+//         }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOneAndUpdate
+
+//         if (!task) {
+//             return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
+//         }
+//         res.status(200).json({ task });
+//     } catch (error) {
+//         res.status(500).json({ msg: error });
+//     }
+//     // res.send('Update task');
+// }
+
+const updateTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!task) {
+        return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
     }
-    // res.json(req.body);
-}
+    res.status(200).json({ task });
+});
 
-const getTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOne({ _id: taskID }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOne
-
-        if (!task) {
-            return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
-        }
-
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ msg: error });
-    }
-    // res.json({ id: req.params.id });
-}
-
-const updateTask = async (req, res) => {
+const editTask = async (req, res) => { // Put request
     try {
         const { id: taskID } = req.params;
         const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
             new: true,
-            runValidators: true
+            runValidators: true,
+            overwrite: true // To overwrite the existing task - To differentiate from patch request
         }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOneAndUpdate
 
         if (!task) {
@@ -54,28 +108,40 @@ const updateTask = async (req, res) => {
     // res.send('Update task');
 }
 
-const deleteTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskID }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOneAndDelete
+// const deleteTask = async (req, res) => {
+//     try {
+//         const { id: taskID } = req.params;
+//         const task = await Task.findOneAndDelete({ _id: taskID }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOneAndDelete
 
-        if (!task) {
-            return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
-        }
+//         if (!task) {
+//             return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
+//         }
 
-        // res.status(200).json({ task });
-        // res.status(200).send();
-        res.status(200).json({ task: null, status: 'success' });
-    } catch (error) {
-        res.status(500).json({ msg: error });
+//         // res.status(200).json({ task });
+//         // res.status(200).send();
+//         res.status(200).json({ task: null, status: 'success' });
+//     } catch (error) {
+//         res.status(500).json({ msg: error });
+//     }
+//     // res.send('Delete a single task');
+// }
+
+const deleteTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID }); // Reference - https://mongoosejs.com/docs/api.html#model_Model-findOneAndDelete
+
+    if (!task) {
+        return res.status(404).json({ msg: `No tasks with id: ${taskID}` });
     }
-    // res.send('Delete a single task');
-}
+
+    res.status(200).json({ task: null, status: 'success' });
+});
 
 module.exports = {
     getAllTasks,
     createTask,
     getTask,
     updateTask,
-    deleteTask
+    deleteTask,
+    editTask
 }
